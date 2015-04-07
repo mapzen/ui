19bc6ef5431a25359e315c47679e7b10fecd4475
Lou Huang <lou@louhuang.com>
2015-04-07 14:12:48 -0400

    More hipness

--- demo/demo3.js
+++ demo/demo3.js
@@ -18,8 +18,8 @@ $(document).on('ready', function (e) {
   var upCounter = 0
   var pageVersion = 1
 
-  if ($body.hasClass('version2')) {
-    pageVersion = 2
+  if ($body.hasClass('version3')) {
+    pageVersion = 3
   }
 
   // nope
@@ -43,13 +43,6 @@ $(document).on('ready', function (e) {
       downCounter++
     }
 
-    if (pageVersion == 2 || pageVersion == 3) {
-      if (windowScrollPos > 260) {
-        $('.product-nav').addClass('show-product-logo')
-      } else {
-        $('.product-nav').removeClass('show-product-logo')
-      }
-    }
 
     // TODO: clear the counters after a certain time has elapsed, if no other scroll action
     // has taken place
@@ -57,8 +50,9 @@ $(document).on('ready', function (e) {
     if (upCounter >= 3) {
       if (!$body.hasClass('is-scrolling')) {
         // this eneds to be done better, timing is off
-        $body.addClass('fix-main-nav')
-        $('nav').addClass('navbar-fixed-top')
+        // disable in this demo
+        //$body.addClass('fix-main-nav')
+        //$('nav').addClass('navbar-fixed-top')
       }
     } else if (downCounter >= 1) {
       $body.removeClass('fix-main-nav')
@@ -66,34 +60,46 @@ $(document).on('ready', function (e) {
     }
 
     //console.log($window.scrollTop())
-    if (windowScrollPos >= (pNavTopPosition - 1)) {
-      $body.addClass("fix-product-nav");
+    if (windowScrollPos >= 200) { //(pNavTopPosition - 1 + 100)) {
+      $body.addClass("fix-floating-nav");
+      $('.floating-container').removeClass('not-floating-yet')
+      $('.product-nav').addClass('show-product-logo')
     } else {
-      $body.removeClass("fix-product-nav");
+      $body.removeClass("fix-floating-nav");
+      $('.floating-container').addClass('not-floating-yet')
+      $('.product-nav').removeClass('show-product-logo')
+    $('.reveal-floating').removeClass('reveal')
     }
 
-    if (windowScrollPos >= sectGithubPos - 60) {
+    if (windowScrollPos >= sectGithubPos -150) {
       $('.top-nav-item').removeClass('active')
       $('#nav-github').addClass('active')
+      $('.navscroller').css('top', -300)
     }
-    else if (windowScrollPos >= sectContributePos - 60) {
+    else if (windowScrollPos >= sectContributePos -150) {
       $('.top-nav-item').removeClass('active')
       $('#nav-contribute').addClass('active')
+      $('.navscroller').css('top', -240)
     }
-    else if (windowScrollPos >= sectApiPos - 60) {
+    else if (windowScrollPos >= sectApiPos -150) {
       $('.top-nav-item').removeClass('active')
       $('#nav-api').addClass('active')
+      $('.navscroller').css('top', -180)
     }
-    else if (windowScrollPos >= sectExamplesPos - 60) {
+    else if (windowScrollPos >= sectExamplesPos -150) {
       $('.top-nav-item').removeClass('active')
       $('#nav-examples').addClass('active')
+      $('.navscroller').css('top', -120)
     }
-    else if (windowScrollPos >= sectHighlightsPos - 60) {
+    else if (windowScrollPos >= sectHighlightsPos -150) {
       $('.top-nav-item').removeClass('active')
       $('#nav-highlights').addClass('active')
+      $('.navscroller').css('top', -60)
     } else {
+      // demo
       $('.top-nav-item').removeClass('active')
       $('#nav-demo').addClass('active')
+      $('.navscroller').css('top', 0)
     }
 
   })
@@ -120,31 +126,44 @@ $(document).on('ready', function (e) {
     momentarilyDisableMainNavReveal()
     $(this).addClass('activated')
     $(window).scrollTo(sectDemoPos, 250, scrollToSettings);
+    $('.navscroller').css('top', 0)
   })
   $('#nav-highlights').on('click', function (e) {
     momentarilyDisableMainNavReveal()
     $(this).addClass('activated')
     $(window).scrollTo(sectHighlightsPos - 60, 250, scrollToSettings);
+    $('.navscroller').css('top', -60)
   })
   $('#nav-examples').on('click', function (e) {
     momentarilyDisableMainNavReveal()
     $(this).addClass('activated')
     $(window).scrollTo(sectExamplesPos - 60, 250, scrollToSettings);
+    $('.navscroller').css('top', -120)
   })
   $('#nav-api').on('click', function (e) {
     momentarilyDisableMainNavReveal()
     $(this).addClass('activated')
     $(window).scrollTo(sectApiPos - 60, 250, scrollToSettings);
+    $('.navscroller').css('top', -180)
   })
   $('#nav-contribute').on('click', function (e) {
     momentarilyDisableMainNavReveal()
     $(this).addClass('activated')
     $(window).scrollTo(sectContributePos - 60, 250, scrollToSettings);
+    $('.navscroller').css('top', -240)
   })
   $('#nav-github').on('click', function (e) {
     momentarilyDisableMainNavReveal()
     $(this).addClass('activated')
     $(window).scrollTo(sectGithubPos - 60, 250, scrollToSettings);
+    $('.navscroller').css('top', -300)
+  })
+
+  $('.floating-container').on('mouseover', function (e) {
+    $('.reveal-floating').addClass('reveal')
+  })
+  $('.reveal-floating').on('mouseout', function (e) {
+    $('.reveal-floating').removeClass('reveal')
   })
 
 })
