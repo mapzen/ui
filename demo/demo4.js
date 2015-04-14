4cd3862b35c44decde948651a232ab0c1a9aa74b
Lou Huang <lou@louhuang.com>
2015-04-14 17:06:19 -0400

    Refactoring

--- demo/demo4.js
+++ demo/demo4.js
@@ -9,8 +9,8 @@ var FIXED_NAV_HIDE_ON_COUNTER = 2
 //var FIXED_NAV_HIDE_AT_Y_POSITION = 50
 var FIXED_NAV_ALWAYS_SHOW_BELOW_Y_POSITION = 50
 
-var PRODUCT_NAV_SELECTOR
 var PRODUCT_NAV_COLLAPSE_DELAY_TIMEOUT = 900
+var PRODUCT_NAV_TOP_OFFSET = 20
 
 var windowPreviousYPosition
 var windowYPosition
@@ -72,69 +72,90 @@ function scrollToSection (element, position) {
   $(window).scrollTo(position, 250, scrollToSettings)
 }
 
-
-var ProductNavigation = function () {
-  this.el = null // Placeholder for product navigation element
-  this.delayedCollapseTimer = null // Placeholder
+var ProductNavigation = function (el) {
+  this.el = document.querySelector(el)
+  this.topPosition = this.el.offsetTop
   this.isFloating = false
+  this.isCollapsed = false
   this.hitboxIsActive = false
-}
+  this.delayedCollapseTimer = null // Placeholder
 
-ProductNavigation.prototype.init = function (el) {
-  var selector = el || PRODUCT_NAV_SELECTOR || '.product-nav'
-  this.el = document.querySelector(selector)
-
-  this.initEvents()
+  this.addEventListeners()
 
   // Enable CSS transition time only after page has completed loading
   // so that setting the navbar into initial state is not animated.
-  setTimeout(function () {
+  document.addEventListener('DOMContentLoaded', function (event) {
     this.el.querySelector('.product-nav-container').classList.add('enable-animation')
-  }.bind(this), 0)
-}
-
-ProductNavigation.prototype.initEvents = function () {
-  // Add event listeners to things.
-  // The hitbox is an invisible area bigger than the navigation bar itself
-  // When the mouse enters this area, expand the navigation bar
-  var hitboxEl = document.querySelector('.product-nav-hitbox')
-
-  hitboxEl.addEventListener('mouseover', this.expand.bind(this))
-  hitboxEl.addEventListener('mouseout', this.delayedCollapse.bind(this))
-  this.el.addEventListener('mouseover', this.expand.bind(this))
-  this.el.addEventListener('mouseout', this.delayedCollapse.bind(this))
-
-  // Collapse nav if mouse re-enters window not on the hitbox area
-  window.addEventListener('mouseover', function (e) {
-    // Do not collapse if navigation is not in the floating state
-    if (this.isFloating === false) return
-
-    if (!e.relatedTarget && !e.target.classList.contains('product-nav-hitbox')) {
-      this.collapse()
-    }
   }.bind(this))
 }
 
+ProductNavigation.prototype.addEventListeners = function () {
+  // The hitbox is an invisible area bigger than the navigation bar itself
+  // When the mouse enters this area, expand the navigation bar
+  var hitboxClassName = 'product-nav-hitbox'
+  var hitboxEl = document.querySelector('.' + hitboxClassName)
+
+  // All the elements that act as hitboxes for the purpose of
+  // expanding product navigation
+  hitboxEl.addEventListener('mouseover', onMouseEntersHitboxArea.bind(this))
+  hitboxEl.addEventListener('mouseout', onMouseLeavesHitboxArea.bind(this))
+  this.el.addEventListener('mouseover', onMouseEntersHitboxArea.bind(this))
+  this.el.addEventListener('mouseout', onMouseLeavesHitboxArea.bind(this))
+  document.querySelector('nav').addEventListener('mouseover', onMouseEntersHitboxArea.bind(this))
+  document.querySelector('nav').addEventListener('mouseout', onMouseLeavesHitboxArea.bind(this))
+
+  window.addEventListener('mouseover', onMouseReentersWindow.bind(this))
+  window.addEventListener('scroll', onScrollWindow.bind(this))
+
+  function onMouseLeavesHitboxArea (event) {
+    this.hitboxIsActive = false
+
+    // event.relatedTarget is null if mouse exits the window
+    // In that case, do not collapse the navigation
+    if (event && !event.relatedTarget) return false
+
+    this.delayedCollapse()
+  }
+
+  function onMouseEntersHitboxArea (event) {
+    this.hitboxIsActive = true
+    this.expand()
+  }
+
+  function onMouseReentersWindow (event) {
+    // Collapse nav if mouse re-enters window not on the hitbox area
+    // Do not collapse if navigation is not in the floating state
+    if (this.isFloating === false) return
+
+    if (!event.relatedTarget && this.hitboxIsActive === false) {
+      this.collapse()
+    }
+  }
+
+  function onScrollWindow (event) {
+    this.determineFloatState()
+  }
+}
+
 ProductNavigation.prototype.collapse = function () {
+  // Never allow a collapse if the hitbox area is active
+  if (this.hitboxIsActive) return
+
+  this.isCollapsed === true
   this.el.classList.add('is-collapsed')
 }
 
 ProductNavigation.prototype.expand = function () {
-  // If there is a delayed collapse timer, clear it
   clearTimeout(this.delayedCollapseTimer)
+  this.isCollapsed === false
   this.el.classList.remove('is-collapsed')
 }
 
-ProductNavigation.prototype.delayedCollapse = function (event) {
-  // event.relatedTarget is null if mouse exits the window
-  // In that case, do not collapse the navigation
-  if (event && !event.relatedTarget) return false
-
-  // Timer priority: local, global, default
+ProductNavigation.prototype.delayedCollapse = function () {
   var timer = PRODUCT_NAV_COLLAPSE_DELAY_TIMEOUT || 1200
 
   // Set a timer to collapse navigation in the future
-  // If the timer has not cleared by then, collapse navigation
+  // This can be canceled by clearing the timeout elsewhere
   this.delayedCollapseTimer = setTimeout(function () {
     if (this.delayedCollapseTimer) {
       this.collapse()
@@ -154,135 +175,136 @@ ProductNavigation.prototype.unfloat = function () {
   document.body.classList.remove('floating-product-nav')
 }
 
+ProductNavigation.prototype.determineFloatState = function () {
+  // Set product navigation to be floated or not
+  // depending on current window position
+  var windowYPosition = window.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop
+
+  if (windowYPosition >= (this.topPosition - PRODUCT_NAV_TOP_OFFSET)) {
+    this.float()
+  } else {
+    this.unfloat()
+    this.expand()
+  }
+}
+
+ProductNavigation.prototype.changeSection = function (newSectionId, triggerCollapse) {
+  var listEls = this.el.querySelectorAll('li')
+  var el = document.getElementById(newSectionId)
+  var elIndex = Array.prototype.indexOf.call(listEls, el)
+  var listScrollPosition = elIndex * 50
+
+  // Remove active class on all list elements
+  for (var i = 0, j = listEls.length; i < j; i++) {
+    listEls[i].classList.remove('active')
+  }
+
+  // Set new section to active state
+  el.classList.add('active')
+
+  // Set list container scroll position for nav in collapsed state
+  el.parentNode.style.top = '-' + listScrollPosition.toString() + 'px'
+
+  // Collapse or expand nav when this section is active?
+  if (triggerCollapse === true && !this.isCollapsed) {
+    this.collapse()
+  } else if (triggerCollapse === false) {
+    this.expand()
+  }
+  // else ignore
+}
+
+var $window = $(window)
+var $body = $('body')
+
+var sectHighlightsPos = $('#section-highlights').offset().top
+var sectExamplesPos = $('#section-examples').offset().top
+var sectApiPos = $('#section-api').offset().top
+var sectContributePos = $('#section-contribute').offset().top
+var sectGithubPos = $('#section-github').offset().top
+var sectFooterPos = $('footer').offset().top
+
+var productNav = new ProductNavigation('#product-nav')
+
+// Start out revealing fixed main nav
+// It already does this, but record the state in DOM
+revealFixedMainNav()
+
+// Set demo height
+setDemoContainerHeight()
+window.addEventListener('resize', setDemoContainerHeight)
+
+$window.on('scroll', function (e) {
+  // Determine window Y positioning
+  // documentElement.scrollTop returns 0 in Chrome for some reason
+  // document.body.scrollTop is deprecated, but is backwards-compatible
+  windowPreviousYPosition = windowYPosition
+  windowYPosition = window.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop
+
+  // Return early if some other script is doing the scrolling, as
+  // indicated by the presence of the `is-scrolling` class that is
+  // momentarily applied to the body element
+  if (document.body.classList.contains('is-scrolling')) {
+    resetScrollCounters()
+    return
+  }
+
+  // Determine window scroll direction
+  // The terms "up" and "down" are from the viewer's perspective.
+  // That is, "scrolling down" means that the page moves _up_ in space
+  // and "scrolling up" means that the page moves _down_ in space.
+  if (windowPreviousYPosition > windowYPosition) {
+    // Scrolling up
+    window.clearTimeout(scrollCounterLifespanTimer)
+    scrollDownCounter = 0
+    scrollUpCounter++
+  } else {
+    // Scrolling down
+    window.clearTimeout(scrollCounterLifespanTimer)
+    scrollUpCounter = 0
+    scrollDownCounter++
+  }
+
+  // Set a timer to expire scroll counter after a set time
+  scrollCounterLifespanTimer = window.setTimeout(function () {
+    resetScrollCounters()
+  }, SCROLL_COUNTER_LIFESPAN)
+
+  // As user scrolls up, reveal main navigation
+  // or hide it, as user scrolls back down
+
+  if (windowYPosition < FIXED_NAV_ALWAYS_SHOW_BELOW_Y_POSITION) {
+    revealFixedMainNav()
+  }
+
+  if (scrollUpCounter >= FIXED_NAV_REVEAL_ON_COUNTER && windowYPosition > FIXED_NAV_ENABLE_AT_Y_POSITION) {
+    revealFixedMainNav()
+  } else if (scrollDownCounter >= FIXED_NAV_HIDE_ON_COUNTER) {
+    hideFixedMainNav()
+  }
+
+  if (windowYPosition >= sectFooterPos) {
+    productNav.changeSection('nav-bottom')
+  } else if (windowYPosition >= sectGithubPos -100) {
+    productNav.changeSection('nav-github')
+  } else if (windowYPosition >= sectContributePos -100) {
+    productNav.changeSection('nav-contribute')
+  } else if (windowYPosition >= sectApiPos -100) {
+    productNav.changeSection('nav-api')
+  } else if (windowYPosition >= sectExamplesPos -100) {
+    productNav.changeSection('nav-examples')
+  } else if (windowYPosition >= sectHighlightsPos -100) {
+    productNav.changeSection('nav-highlights', true)
+  } else {
+    productNav.changeSection('nav-demo', false)
+  }
+})
+
 $(document).on('ready', function (e) {
-  var $window = $(window)
-  var $body = $('body')
-  var $pNav = $('#product-nav')
-  var pNavTopPosition = $pNav.offset().top
-  var pNavHeight = $pNav.height()
-
-  var sectDemoPos = $('#demo-container').position().top
-  var sectHighlightsPos = $('#section-highlights').offset().top
-  var sectExamplesPos = $('#section-examples').offset().top
-  var sectApiPos = $('#section-api').offset().top
-  var sectContributePos = $('#section-contribute').offset().top
-  var sectGithubPos = $('#section-github').offset().top
-  var sectFooterPos = $('footer').offset().top
-
-  var productNav = new ProductNavigation()
-
-  // Start out revealing fixed main nav
-  // It already does this, but record the state in DOM
-  revealFixedMainNav()
-
-  // Set demo height
-  setDemoContainerHeight()
-  window.addEventListener('resize', setDemoContainerHeight)
-
-  productNav.init()
-
-  $window.on('scroll', function (e) {
-    // Determine window Y positioning
-    // documentElement.scrollTop returns 0 in Chrome for some reason
-    // document.body.scrollTop is deprecated, but is backwards-compatible
-    windowPreviousYPosition = windowYPosition
-    windowYPosition = window.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop
-
-    // Return early if some other script is doing the scrolling, as
-    // indicated by the presence of the `is-scrolling` class that is
-    // momentarily applied to the body element
-    if (document.body.classList.contains('is-scrolling')) {
-      resetScrollCounters()
-      return
-    }
-
-    // Determine window scroll direction
-    // The terms "up" and "down" are from the viewer's perspective.
-    // That is, "scrolling down" means that the page moves _up_ in space
-    // and "scrolling up" means that the page moves _down_ in space.
-    if (windowPreviousYPosition > windowYPosition) {
-      // Scrolling up
-      window.clearTimeout(scrollCounterLifespanTimer)
-      scrollDownCounter = 0
-      scrollUpCounter++
-    } else {
-      // Scrolling down
-      window.clearTimeout(scrollCounterLifespanTimer)
-      scrollUpCounter = 0
-      scrollDownCounter++
-    }
-
-    // Set a timer to expire scroll counter after a set time
-    scrollCounterLifespanTimer = window.setTimeout(function () {
-      resetScrollCounters()
-    }, SCROLL_COUNTER_LIFESPAN)
-
-    // As user scrolls up, reveal main navigation
-    // or hide it, as user scrolls back down
-
-    if (windowYPosition < FIXED_NAV_ALWAYS_SHOW_BELOW_Y_POSITION) {
-      revealFixedMainNav()
-    }
-
-    if (scrollUpCounter >= FIXED_NAV_REVEAL_ON_COUNTER && windowYPosition > FIXED_NAV_ENABLE_AT_Y_POSITION) {
-      revealFixedMainNav()
-    } else if (scrollDownCounter >= FIXED_NAV_HIDE_ON_COUNTER) {
-      hideFixedMainNav()
-    }
-
-    // 20 is the distance away from top of screen
-    if (windowYPosition >= (pNavTopPosition - 20)) {
-      productNav.float()
-    } else {
-      productNav.unfloat()
-      productNav.expand()
-    }
-
-    if (windowYPosition >= sectFooterPos) {
-      $('.product-nav-item').removeClass('active')
-      $('#nav-bottom').addClass('active')
-      $('.product-nav-scroll-container').css('top', -300)
-    }
-    else if (windowYPosition >= sectGithubPos -100) {
-      $('.product-nav-item').removeClass('active')
-      $('#nav-github').addClass('active')
-      $('.product-nav-scroll-container').css('top', -250)
-    }
-    else if (windowYPosition >= sectContributePos -100) {
-      $('.product-nav-item').removeClass('active')
-      $('#nav-contribute').addClass('active')
-      $('.product-nav-scroll-container').css('top', -200)
-    }
-    else if (windowYPosition >= sectApiPos -100) {
-      $('.product-nav-item').removeClass('active')
-      $('#nav-api').addClass('active')
-      $('.product-nav-scroll-container').css('top', -150)
-    }
-    else if (windowYPosition >= sectExamplesPos -100) {
-      $('.product-nav-item').removeClass('active')
-      $('#nav-examples').addClass('active')
-      $('.product-nav-scroll-container').css('top', -100)
-    }
-    else if (windowYPosition >= sectHighlightsPos -100) {
-      $('.product-nav-item').removeClass('active')
-      $('#nav-highlights').addClass('active')
-      $('.product-nav-scroll-container').css('top', -50)
-
-      productNav.collapse()
-    } else {
-      // demo
-      $('.product-nav-item').removeClass('active')
-      $('#nav-demo').addClass('active')
-      $('.product-nav-scroll-container').css('top', 0)
-
-      productNav.expand()
-    }
-
-  })
 
   // Clicks
   $('#nav-demo').on('click', function (e) {
-    scrollToSection(this, sectDemoPos -100)
+    scrollToSection(this, 0)
     $('.product-nav-item').removeClass('active')
     $('.product-nav-scroll-container').css('top', 0)
   })
