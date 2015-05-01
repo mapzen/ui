c0275345646edc0462acdc36825b8f8790630ca8
Lou Huang <lou@louhuang.com>
2015-05-01 14:01:57 -0400

    Add optimized logo images to CDN, remove image border in IE10

--- components/bug/bug.js
+++ components/bug/bug.js
@@ -32,16 +32,13 @@ var MapzenBug = (function () {
     this.el = this.createElAndAppend()
   }
 
-  MapzenBug.prototype.init = function () {
-
-  }
-
   MapzenBug.prototype.createElAndAppend = function () {
     var el = document.createElement('div')
     var link = document.createElement('a')
     var img = document.createElement('img')
 
-    el.id = 'mazpen-bug' // Just be glad I didn't call it pamnez
+    el.id = 'mz-bug'
+    el.className = 'mz-bug-container'
     el.style.boxSizing = 'border-box'
     el.style.position = 'fixed'
     el.style.top = '0'
@@ -56,8 +53,9 @@ var MapzenBug = (function () {
     el.style.zIndex = 10010 // Mapzen zip code
 
     link.href = this.opts.link || 'https://mapzen.com/'
-    link.style.boxSizing = 'border-box'
     link.target = '_blank'
+    link.className = 'mz-bug-anchor'
+    link.style.boxSizing = 'border-box'
     link.style.display = 'block'
     link.style.position = 'absolute'
     link.style.top = 0
@@ -66,8 +64,10 @@ var MapzenBug = (function () {
     link.style.height = '100%'
     link.style.padding = '8px'
 
-    img.src = 'https://mapzen.com/assets/logos/mapzen-logo-square-color-lit.png'
+    img.src = 'https://cdn.rawgit.com/mapzen/ui/master/components/bug/mz-logo-lit-64@2x.png'
+    img.className = 'mz-bug-logo'
     img.style.width = '64px'
+    img.style.border = 0
 
     link.appendChild(img)
     el.appendChild(link)
